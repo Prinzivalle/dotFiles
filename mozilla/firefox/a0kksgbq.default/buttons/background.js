e95b3ececa8f110a954b211eb2ce1370b35a0ecd
Prinzivalle <riccardoprinzi1997@gmail.com>
2020-02-10 22:02:31 +0100

    Added mozilla config for customization

---
+++ mozilla/firefox/a0kksgbq.default/buttons/background.js
@@ -0,0 +1,11 @@
+browser.browserAction.onClicked.addListener(async() => {
+	try {
+		browser.windows.remove((await browser.windows.getCurrent({ windowTypes: ['normal'] })).id)
+	} catch (err) { console.error(err) }
+})
+
+/*browser.browserAction.onClicked.addListener(async() => {
+	
+		browser.windows.remove((await browser.windows.getCurrent({ windowTypes: ['normal'] })).id)
+	 
+})*/
